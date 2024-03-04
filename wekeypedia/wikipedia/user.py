f28ef2cb79b996364bfb264d58807874ec3d9fca
duong tam kien <tk@renard.ninja>
2024-03-04 23:43:09 +0100

    ajoute une méthode pour récupérer l'ensemble des diffs d'un `User`

--- wekeypedia/wikipedia/user.py
+++ wekeypedia/wikipedia/user.py
@@ -20,6 +20,7 @@ class WikipediaUser:
     self.lang = lang
     self.name = name
 
+
   def fetch_contribs(self):
     """ get all contributions from a user """
     api = API(lang=self.lang)
@@ -38,6 +39,36 @@ class WikipediaUser:
     while True:
       r = api.get(params)
       contribs += r["query"]["usercontribs"]
+
+      if "continue" in r:
+#        print r["continue"]
+        params.update(r["continue"])
+      else:
+        break
+
+    return contribs
+
+ 
+  def fetch_contribs_full(self):
+    """ get all contributions from a user """
+    api = API(lang=self.lang)
+
+    contribs = []
+
+    params = {
+      "action":"query",
+      "format": "json",
+      "list":"allrevisions",
+      "arvuser": self.name,
+      "arvlimit": "50",
+      "arvprop": "user|userid|timestamp|size|ids|sha1|comment",
+      "arvdiffto": "prev",
+      "continue": ""
+    }
+
+    while True:
+      r = api.get(params)
+      contribs += r["query"]["allrevisions"]
 
       if "continue" in r:
 #        print r["continue"]
